6211c824a5f69fc3bd1f2a9538cc89f758c90854
Doyoudo丶 <728494186@qq.com>
2021-01-12 23:31:51 +0800

    更改正式环境路径

--- build/webpack-config/webpack.output.js
+++ build/webpack-config/webpack.output.js
@@ -7,7 +7,7 @@ module.exports = (production = false) => {
           path: path.resolve(__dirname, '../../dist/admin'),
           filename: 'static/[name]/index.[chunkhash].js',
           chunkFilename: 'static/[name].[id].[chunkhash].js',
-          publicPath: 'http://8.129.90.25:9000/admin',
+          publicPath: 'http://8.129.90.25:9000/admin/',
           library: 'managePermission',
           libraryTarget: 'umd'
         }
@@ -15,7 +15,7 @@ module.exports = (production = false) => {
           path: path.resolve(__dirname, '../../dist'),
           filename: '[name].js',
           chunkFilename: '[name].js',
-          publicPath: 'http://127.0.0.1:3000/admin',
+          publicPath: 'http://127.0.0.1:3000/admin/',
           library: 'managePermission',
           libraryTarget: 'umd'
         }
